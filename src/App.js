aca282aaabe145f042f1148467788dd611ee61cf
Artem Titov <artem.titov@leomax.ru>
2019-04-07 23:35:14 +0300

    Update

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import './App.css';
+import PointsList from "./PointsList";
+class App extends Component {
+    state = {
+        referencePoints: [],
+        order: [],
+        inputAddress: '',
+        routeIsCreated: false,
+    };
+
+    componentDidMount() {
+        window.addEventListener('load', this.handleLoad);
+    };
+
+    handleLoad = () => {
+        window.ymaps.ready(() => {
+            this.localMap = new window.ymaps.Map('map', {center: [55.734876, 37.59308], zoom: 10,},);
+
+        });
+
+
+    };
+
+    handleInput = (ev) => {
+        this.setState({
+            inputAddress : ev.target.value
+        })
+    };
+
+    createRoute = () => {
+        const {inputAddress} = this.state;
+        this.multiRoute = new window.ymaps.multiRouter.MultiRoute({
+                referencePoints: [inputAddress]
+                ,
+                params: {
+                    routingMode: 'auto',
+                    reverseGeocoding: true
+                }
+            }
+            ,{
+                boundsAutoApply: true,
+                balloonLayout: this.balloonLayout,
+            });
+
+        this.multiRoute.editor.start();
+
+        this.multiRoute.model.events.add('requestsuccess', () => {
+
+            window.waypoints=this.multiRoute.getWayPoints()
+
+            this.multiRoute.getWayPoints().each((point, key)=>{
+                window.ymaps.geoObject.addon.balloon.get(point);
+                point.properties.set({
+                    balloonContent:`Точка маршрута ${key+1}`,
+                });
+            });
+
+
+            this.multiRoute.events.add("boundschange", () => {
+
+              const {referencePoints} = this.state;
+
+               if(referencePoints.length!== 0) {
+                   this.localMap.setBounds( this.multiRoute.getBounds(), {
+                       checkZoomRange: true
+                   });
+               }
+            });
+
+            const newReferencePoints =  this.multiRoute.model.properties._data.waypoints;
+            const {order} = this.state;
+
+            let  newOrder;
+            if(order.length!==0 && newReferencePoints.length!== order.length) {
+                newOrder = order.concat([order.length])
+            }
+            else {
+                newOrder = newReferencePoints.map((point, i) => i);
+            }
+            this.setState({referencePoints: newReferencePoints, order: newOrder, routeIsCreated: true})
+
+        });
+
+        this.localMap.geoObjects.add(this.multiRoute);
+    };
+
+    addPoint = (ev) => {
+        ev.preventDefault();
+        const {routeIsCreated, inputAddress, referencePoints} = this.state;
+
+        if (!routeIsCreated) {
+          this.createRoute();
+        }
+
+        else {
+          const coordinatesOfReferencePoints = referencePoints.map( point => point.coordinates.reverse());
+          coordinatesOfReferencePoints.push(inputAddress);
+          this.multiRoute.model.setReferencePoints(coordinatesOfReferencePoints)
+        }
+
+        this.setState({inputAddress: '',})
+    };
+
+    removePoint = deletePoint => () => {
+        const {referencePoints} = this.state;
+        const newReferencePoints =  referencePoints.filter(point => point.coordinates!== deletePoint.coordinates);
+        const newOrder = newReferencePoints.map( (point, i) => i);
+        this.setState({referencePoints: newReferencePoints, order: newOrder}, this.updateRoute)
+    }
+
+    updateRoute = () => {
+        const {referencePoints} = this.state;
+        const coordinatesOfReferencePoints = referencePoints.map( point => point.coordinates.reverse());
+        this.multiRoute.model.setReferencePoints(coordinatesOfReferencePoints)
+    };
+
+    render() {
+        const {referencePoints, inputAddress, order} = this.state;
+        console.log(order);
+        console.log(referencePoints);
+        return (
+          <div className='app-container'>
+              <div>
+                  <form className='app-form'>
+                      <input
+                          className='form-input'
+                          type="text"
+                          autoFocus
+                          placeholder='Новая точка маршрута'
+                          onChange={this.handleInput}
+                          value={inputAddress}
+                      />
+                      <button className='form-button'  onClick={this.addPoint}>Добавить</button>
+                  </form>
+                  <PointsList
+                      removePoint={this.removePoint}
+                      order={order}
+                      referencePoints={referencePoints}
+                      onChange={(order) => {
+                          this.setState({ order });
+                      }}
+                  >
+                  </PointsList>
+              </div>
+              <div id='map' className='map'>
+              </div>
+          </div>
+        );
+  }
+}
+
+export default App;
